75319792ec9532bfa62acc1fec1742840fb3acfc
henanja <henanja@users.noreply.github.com>
2026-04-18 12:24:46 +0800

    Fix: User model missing UserMixin for Flask-Login

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 from datetime import datetime
 from config import Config
 
@@ -92,7 +93,7 @@ class Inquiry(db.Model):
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
 # 用户/管理员
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
